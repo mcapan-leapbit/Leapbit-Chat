6b9146075aba8306e11a32f9963df8c464aabbfd
Fran <fran.belac@gmail.com>
2022-07-05 15:19:55 +0200

    gulp setup complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('stilovi/**/*.scss')
+    return src('public/sass/**/*.scss')
         .pipe(sass())
-        .pipe(dest('css'))
+        .pipe(dest('public/styles'))
 }
 
 function watchTask () {
-    watch(['stilovi/**/*.scss'],buildStyles)
+    watch(['public/sass/**/*.scss'],buildStyles)
 }
 
 exports.default = series(buildStyles,watchTask)
