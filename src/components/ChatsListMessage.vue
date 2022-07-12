520a4680451d35594f0a15feb6c7b2a0576b7641
Marin Capan <marin.capan@leapbit.com>
2022-07-12 10:29:42 +0200

    added chats list component

---
+++ src/components/ChatsListMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="message-box">
+              <div class="">
+                <div class="chat-sender">
+                  <span class="profile-bg"
+                    ><span class="profile-initials">AW</span></span
+                  >
+                  <div class="sender-details">
+                    <div class="profile-name">
+                      Austin Wade
+                      <span class="message-timestamp"
+                        >July 10, 2022 | 08:45</span
+                      >
+                    </div>
+                    <span class="profile-email">austin.wade@leapbit.com</span>
+                  </div>
+                </div>
+              </div>
+              <div class="chat-bottom">
+                <p class="message-text">
+                  Vivamus vel gravida urna, in hendrerit est. Nunc suscipit
+                  lectus eget nibh convallis malesuada. Fusce ultrices ultricies
+                  pulvinar…
+                </p>
+              </div>
+            </div>
+</template>
+
+<script>
+export default {
+    name: "Sidebar",
+}
+</script>
+
