332f3810905e4d6b2e4626a740fb2c48ef1fffd2
Marin Capan <marin.capan@leapbit.com>
2022-07-14 07:55:24 +0200

    added config file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/router");
-const dotenv = require("dotenv");
 
-dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
