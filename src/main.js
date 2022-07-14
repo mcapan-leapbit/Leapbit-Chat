9b473335541dfb606249d177fe4d7b7ad031df47
josip555 <josip.arelic00@gmail.com>
2022-07-14 15:11:36 +0200

    fixing

--- src/main.js
+++ src/main.js
@@ -2,17 +2,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import cookie from "vue-cookie";
-import uuid from "vue-uuid";
 import VueSocketIOExt from "vue-socket.io-extended";
 import { io } from "socket.io-client";
+import { VueCookie } from "vue-cookie";
+import { uuid } from "vue-uuid";
 
 const socket = io("http://localhost:3000");
 
 const vue = createApp(App).use(store).use(router);
 
-vue.use(cookie);
+vue.use(VueCookie);
 vue.use(uuid);
+
 vue.use(VueSocketIOExt, socket);
 //vue.config.globalProperties.$socket = socket;
 vue.mount("#app");
