ecdb21e0ec6b36632cf97504f6735e2a3aa2f842
josip555 <josip.arelic00@gmail.com>
2022-07-15 12:33:14 +0200

    added initials() formating function

--- src/components/ChatsList.vue
+++ src/components/ChatsList.vue
@@ -1,50 +1,52 @@
 <template>
   <div class="chats-list">
-          <div class="chats-top">
-            <div class="search-pack">
-              <div class="search-pack-left">
-                <span class="search-label">Search for chat</span>
-                <input class="search-box" type="text" placeholder="Search" />
-              </div>
-            </div>
-            <div class="active-box">
-              <span class="status-label">Status</span>
-              <div class="status-box">
-                <span class="status-icon"></span>
-                <span class="status-placeholder">Active</span>
-                <img src="../../public/assets/images/icon-chevron.svg" class="status-chevron" />
-              </div>
-            </div>
-          </div>
-          <div class="status-divider"></div>
-          <div class="scrollable-messages">
-            <ChatsListMessage 
-                v-for="admin_message in admin_messages"
-                :key="admin_message.id"
-                :initials="admin_message.initials"
-                :full_name="admin_message.full_name"
-                :timestamp="admin_message.timestamp"
-                :email="admin_message.email"
-                :message="admin_message.message_text"
-                :notif_number="admin_message.new_message"
-            />
-     </div>
+    <div class="chats-top">
+      <div class="search-pack">
+        <div class="search-pack-left">
+          <span class="search-label">Search for chat</span>
+          <input class="search-box" type="text" placeholder="Search" />
         </div>
+      </div>
+      <div class="active-box">
+        <span class="status-label">Status</span>
+        <div class="status-box">
+          <span class="status-icon"></span>
+          <span class="status-placeholder">Active</span>
+          <img
+            src="../../public/assets/images/icon-chevron.svg"
+            class="status-chevron"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="status-divider"></div>
+    <div class="scrollable-messages">
+      <ChatsListMessage
+        v-for="admin_message in admin_messages"
+        :key="admin_message.id"
+        :full_name="admin_message.full_name"
+        :timestamp="admin_message.timestamp"
+        :email="admin_message.email"
+        :message="admin_message.message_text"
+        :notif_number="admin_message.new_message"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-    import ChatsListMessage from "../../src/components/ChatsListMessage.vue";
-    import admin_messages from "../../src/assets/admin_messages.json";
+import ChatsListMessage from "../../src/components/ChatsListMessage.vue";
+import admin_messages from "../../src/assets/admin_messages.json";
 
-    export default {
-        name: "ChatsList",
-        components: {
-            ChatsListMessage,
-        },
-        data(){
-            return{
-                admin_messages: admin_messages
-            };
-        },
+export default {
+  name: "ChatsList",
+  components: {
+    ChatsListMessage,
+  },
+  data() {
+    return {
+      admin_messages: admin_messages,
     };
+  },
+};
 </script>
--- src/components/ChatsListMessage.vue
+++ src/components/ChatsListMessage.vue
@@ -1,39 +1,51 @@
 <template>
   <div class="message-box">
-              <div class="">
-                <div class="chat-sender">
-                  <span class="profile-bg">
-                    <span class="profile-initials">{{initials}}
-                    <span v-if="isValid" class="notif-bg">{{notif_number}}</span>
-                    </span>
-                  </span>
-                  <div class="sender-details">
-                    <div class="profile-name">
-                      {{full_name}}
-                      <span class="message-timestamp"
-                        >{{timestamp}}</span>
-                    </div>
-                    <span class="profile-email">{{email}}</span>
-                  </div>
-                </div>
-              </div>
-              <div class="chat-bottom">
-                <p class="message-text">
-                  {{message}}
-                </p>
-              </div>
-            </div>
+    <div class="">
+      <div class="chat-sender">
+        <span class="profile-bg">
+          <span class="profile-initials"
+            >{{ initials }}
+            <span v-if="isValid" class="notif-bg">{{ notif_number }}</span>
+          </span>
+        </span>
+        <div class="sender-details">
+          <div class="profile-name">
+            {{ full_name }}
+            <span class="message-timestamp">{{ timestamp }}</span>
+          </div>
+          <span class="profile-email">{{ email }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="chat-bottom">
+      <p class="message-text">
+        {{ message }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ChatsListMessage",
-    props: ['initials','full_name','timestamp','email','message','notif_number'],
-    computed: {
-        isValid() {      
-            return (this.notif_number !== null)  && (this.notif_number !== '') && (+this.notif_number > 0);      
-        }
-    }
-}
-</script>
+  name: "ChatsListMessage",
+  props: ["full_name", "timestamp", "email", "message", "notif_number"],
+  computed: {
+    isValid() {
+      return (
+        this.notif_number !== null &&
+        this.notif_number !== "" &&
+        +this.notif_number > 0
+      );
+    },
+    initials() {
+      let names = this.full_name.split(" "),
+        initials = names[0].substring(0, 1).toUpperCase();
 
+      if (names.length > 1) {
+        initials += names[names.length - 1].substring(0, 1).toUpperCase();
+      }
+      return initials;
+    },
+  },
+};
+</script>
