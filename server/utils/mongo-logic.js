3818f3b38cb52702d14fd33951dfd5c4b2e24f58
Marin Capan <marin.capan@leapbit.com>
2022-07-15 14:22:21 +0200

    added method for irl data

--- server/utils/mongo-logic.js
+++ server/utils/mongo-logic.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config({path:'../.env'});
 
 const connectToDB = async function () {
-  console.log(process.env);
   const client = await mongodb.MongoClient.connect(
     process.env.MONGO_LINK,
     {
