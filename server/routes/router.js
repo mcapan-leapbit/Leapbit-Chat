adfd27c8e7ae68a1a192d19a675021d58da9dae6
Marin Capan <marin.capan@leapbit.com>
2022-07-18 10:26:22 +0200

    fixed vue plugin warn

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { connectToDB } = require("../utils/mongo-logic");
+const connectToDB = require("../utils/mongo-logic");
 
 let connection;
 
