cfb5b79ba8b29783590fefc96c1ed424d75b98be
Marin Capan <marin.capan@leapbit.com>
2022-07-18 14:36:13 +0200

    added function for auto notif

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,17 +10,17 @@ let connection;
 
 router.get("/conversation/:id", async (req, res) => {
   const conversation_id = req.params.id;
-  const messages = await connection.db("leapbitChat").collection("messages_2");
+  const messages = await connection.db("leapbitChat").collection("messages");
   res.send(await messages.findOne({ conversation_id: conversation_id }));
 });
 
 router.get("/conversations", async (req, res) => {
-  const messages = await connection.db("leapbitChat").collection("messages_2");
+  const messages = await connection.db("leapbitChat").collection("messages");
   res.send(await messages.find({}).toArray());
 });
 
 router.post("/message", async (req, res) => {
-  const messages = await connection.db("leapbitChat").collection("messages_2");
+  const messages = await connection.db("leapbitChat").collection("messages");
   await messages.updateOne(
     { conversation_id: req.body.conversation_id },
     req.body.values,
