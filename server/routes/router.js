c47382512e527dc23cebe47bee5e84515d2c9f2b
Marin Capan <marin.capan@leapbit.com>
2022-07-18 11:18:50 +0200

    fixed import error

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const connectToDB = require("../utils/mongo-logic");
+const { connectToDB } = require("../utils/mongo-logic");
 
 let connection;
 
@@ -10,17 +10,17 @@ let connection;
 
 router.get("/conversation/:id", async (req, res) => {
   const conversation_id = req.params.id;
-  const messages = await connection.db("leapbitChat").collection("messages");
+  const messages = await connection.db("leapbitChat").collection("messages_2");
   res.send(await messages.findOne({ conversation_id: conversation_id }));
 });
 
 router.get("/conversations", async (req, res) => {
-  const messages = await connection.db("leapbitChat").collection("messages");
+  const messages = await connection.db("leapbitChat").collection("messages_2");
   res.send(await messages.find({}).toArray());
 });
 
 router.post("/message", async (req, res) => {
-  const messages = await connection.db("leapbitChat").collection("messages");
+  const messages = await connection.db("leapbitChat").collection("messages_2");
   await messages.updateOne(
     { conversation_id: req.body.conversation_id },
     req.body.values,
